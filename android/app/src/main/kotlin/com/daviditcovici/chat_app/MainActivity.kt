2f3a579f95c16ed61a8d603353f7b67b9559f952
David Itcovici <daviditco4@gmail.com>
2021-08-19 22:24:04 -0300

    Allow notifications with my custom sound for devices running Android API 26 or newer by creating a notification channel

--- android/app/src/main/kotlin/com/daviditcovici/chat_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/daviditcovici/chat_app/MainActivity.kt
@@ -1,6 +1,49 @@
 package com.daviditcovici.chat_app
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.media.AudioAttributes
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val soundUri: Uri =
+                    Uri.parse(
+                            "android.resource://" +
+                                    applicationContext.packageName +
+                                    "/" +
+                                    R.raw.ringtone
+                    )
+
+            val audioAttributes =
+                    AudioAttributes.Builder()
+                            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                            .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
+                            .build()
+
+            val channel =
+                    NotificationChannel(
+                            "chat_notification_channel",
+                            "chat_message_notification",
+                            NotificationManager.IMPORTANCE_HIGH
+                    )
+            channel.setSound(soundUri, audioAttributes)
+
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
